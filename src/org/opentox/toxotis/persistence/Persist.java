86be28cfd71fe0a1022884337dcb3c627fe0dc24
Sopasakis Pantelis <chvng@mail.ntua.gr>
2010-12-01 03:57:07 +0200

    All configuration files moved outside the source code in ./config

--- src/org/opentox/toxotis/persistence/Persist.java
+++ src/org/opentox/toxotis/persistence/Persist.java
@@ -29,7 +29,7 @@ public class Persist {
 
     public static void main(String[] args) throws Exception {
         org.apache.log4j.LogManager.resetConfiguration();
-        org.apache.log4j.PropertyConfigurator.configure("src/org/opentox/toxotis/persistence/config/log4j.properties");
+        org.apache.log4j.PropertyConfigurator.configure("config/log4j.properties");
 
         SessionFactory sf = HibernateUtil.getSessionFactory();
         Session session = sf.openSession();
