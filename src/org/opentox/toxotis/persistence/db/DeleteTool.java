68836a50982d4ca432f4eeaa8be1f7eb4b949f4e
Sopasakis Pantelis <chvng@mail.ntua.gr>
2010-12-13 14:18:47 +0200

    Added deletion functionality for task resources given their URI

--- src/org/opentox/toxotis/persistence/db/DeleteTool.java
+++ src/org/opentox/toxotis/persistence/db/DeleteTool.java
@@ -1,6 +1,7 @@
 package org.opentox.toxotis.persistence.db;
 
 import org.hibernate.Session;
+import org.opentox.toxotis.client.VRI;
 import org.opentox.toxotis.core.component.Task;
 
 /**
@@ -11,6 +12,35 @@ import org.opentox.toxotis.core.component.Task;
 public class DeleteTool {
 
     private static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DeleteTool.class);
+
+    /**
+     * Deletes a task of a given URI from the database
+     * @param session
+     * @param taskUri
+     */
+    public static void deleteTask(Session session, VRI taskUri) {
+        Task t = new Task(taskUri);
+        t.setMeta(null);
+        t.setCreatedBy(null);
+        t.setResultUri(null);
+        t.setStatus(null);
+        try {
+            session.beginTransaction();
+            session.delete(t);
+            session.getTransaction().commit();
+        } catch (RuntimeException ex) {
+            logger.warn("Deletion of task with URI " + taskUri
+                    + " failed. Logging the corresponding exception for details", ex);
+            try {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            } catch (Throwable rte) {
+                logger.error("Cannot rollback", rte);
+            }
+            throw ex;
+        }
+    }
 
     public static void deleteTask(Session session, Task.Status... status) {
         try {
